584f2067259eaf343a7ea808211e38f1dcdf1c4e
Reilly Chase <rchase@rchase.com>
2024-02-26 09:49:45 -0500

    Add support for locating abuse forms

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+from urllib.parse import quote
 import argparse
 import os
 
@@ -13,6 +14,13 @@ args = parser.parse_args()
 
 client = Client()
 
-number = client.lookups.v2.phone_numbers(args.number).fetch(fields='line_type_intelligence')
+number_info = client.lookups.v2.phone_numbers(args.number).fetch(fields='line_type_intelligence')
+carrier = number_info.line_type_intelligence["carrier_name"]
 
-print(number.line_type_intelligence["carrier_name"])
+print("The spammer's carrier is: " + carrier)
+if "Twilio" in carrier:
+    print("You can report them here: https://www.twilio.com/en-us/help/abuse")
+
+else:
+    target = "https://google.com/search?q=" + quote(carrier + " SMS spam abuse report form")
+    print("Unable to find the abuse report form but LMGTFY: " + target)
